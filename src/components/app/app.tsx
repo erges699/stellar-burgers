bff824617ede0a242a04e6cd5ce55883209789f9
Serge <erges699@gmail.com>
2025-08-03 13:03:53 +0300

    20250803-01

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,42 @@
-import { ConstructorPage } from '@pages';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import {
+  AppHeader,
+  Modal,
+  OrderInfo,
+  IngredientDetails
+} from '@components';
+
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Preloader } from '../ui/preloader';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+    </Routes>
+    
   </div>
 );
 
