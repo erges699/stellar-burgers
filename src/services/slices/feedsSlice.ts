136b3c45a1cfe938f71a5b0c195998e47af1b1ce
Serge <erges699@gmail.com>
2025-08-16 16:46:10 +0300

    20250816 slices

---
+++ src/services/slices/feedsSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { TOrder } from '@utils-types';
+import { getFeedsApi } from '@api';
+
+type TFeedsState = {
+  orders: TOrder[];
+  total: number;
+  totalToday: number;
+  isLoading: boolean;
+  error: string | null;
+};
+
+const initialState: TFeedsState = {
+  orders: [],
+  total: 0,
+  totalToday: 0,
+  isLoading: false,
+  error: null
+};
+
+export const getFeedsApiAsync = createAsyncThunk('feeds/getFeeds', getFeedsApi);
+
+export const feedsSlice = createSlice({
+  name: 'feeds',
+  initialState,
+  reducers: {},
+  selectors: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getFeedsApiAsync.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getFeedsApiAsync.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || null;
+      })
+      .addCase(getFeedsApiAsync.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orders = action.payload.orders;
+        state.total = action.payload.total;
+        state.totalToday = action.payload.totalToday;
+      });
+  }
+});
+
+export default feedsSlice.reducer;
---
+++ src/services/slices/ingredientsSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { TIngredient } from '@utils-types';
+import { getIngredientsApi } from '@api';
+
+type TIngredientsState = {
+  ingredients: TIngredient[];
+  isLoading: boolean;
+  error: string | null;
+};
+
+const initialState: TIngredientsState = {
+  ingredients: [],
+  isLoading: false,
+  error: null
+};
+
+export const getIngredientsAsync = createAsyncThunk(
+  'ingredients/getIngredients',
+  getIngredientsApi
+);
+
+export const ingredientsSlice = createSlice({
+  name: 'ingredients',
+  initialState,
+  reducers: {},
+  selectors: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getIngredientsAsync.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getIngredientsAsync.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || null;
+      })
+      .addCase(getIngredientsAsync.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.ingredients = action.payload;
+      });
+  }
+});
+
+export default ingredientsSlice.reducer;
